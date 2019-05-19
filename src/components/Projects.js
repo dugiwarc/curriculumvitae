9bfc177c26b6c5b3ad1140107bd85ea70a48beb1
dugi <contact@tusk.world>
2019-05-19 13:40:33 +0200

    add presentation modal

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./Projects.css";
 
-// import img_1 from "../imgs/1.png";
-// import img_2 from "../imgs/2.png";
-// import img_3 from "../imgs/3.png";
-// import img_4 from "../imgs/4.png";
+import img_1 from "../imgs/1.png";
+import img_2 from "../imgs/2.png";
+import img_3 from "../imgs/3.png";
+import img_4 from "../imgs/4.png";
 
 // import img_5 from "../imgs/5.png";
 // import img_6 from "../imgs/6.png";
@@ -16,61 +16,123 @@ export default class Projects extends Component {
   constructor() {
     super();
     this.state = {
-      isHidden: true
+      isHidden: true,
+      isClicked: false,
+      sampleT: "translateX(160px)",
+      xCoord: 160
     };
   }
 
   handleClick = () => {
     console.log("The clock is ticking");
     this.setState({
-      isHidden: !this.state.isHidden
+      isHidden: !this.state.isHidden,
+      isClicked: false
     });
   };
+
+  handleCircle = () => {
+    this.setState({
+      isClicked: true
+    });
+  };
+
+  moveRight = () => {
+    let updatedCoords = this.state.xCoord + 315;
+    console.log(updatedCoords);
+    this.setState({
+      sampleT: `translateX(${updatedCoords}px)`,
+      xCoord: updatedCoords
+    });
+  };
+
+  moveLeft = () => {
+    let updatedCoords = this.state.xCoord - 315;
+    console.log(updatedCoords);
+    this.setState({
+      sampleT: `translateX(${updatedCoords}px)`,
+      xCoord: updatedCoords
+    });
+  };
+
   render() {
-    const stylesCircle = this.state.isHidden ? "circle" : "circle show";
+    const stylesCircle = this.state.isHidden
+      ? "circle"
+      : this.state.isClicked
+      ? "circle show presentation-box"
+      : "circle show";
+
+    const stylesPresentation = this.state.isClicked
+      ? "project-images reveal"
+      : "project-images";
+
+    const actionStyles = {
+      transform: this.state.sampleT
+    };
+
     return (
       <div className="Project">
         <hr />
-        <div className="Project-container" onClick={this.handleClick}>
-          <img src={rocketImg} height={100} alt="projects" />
-          <div className={stylesCircle}>
-            <div className="text">Nothing here yet.</div>
+        <div className="Project-container">
+          <img
+            src={rocketImg}
+            height={100}
+            alt="projects"
+            onClick={this.handleClick}
+          />
+          <div className={stylesCircle} onClick={this.handleCircle}>
+            <div className="Project-images">
+              <div
+                onClick={this.moveRight}
+                className={this.state.isClicked ? "right-button" : ""}
+              />
+              <div
+                onClick={this.moveLeft}
+                className={this.state.isClicked ? "left-button" : ""}
+              />
+              <img
+                style={actionStyles}
+                className={stylesPresentation}
+                src={img_1}
+                alt="1"
+              />
+              <img
+                style={actionStyles}
+                className={stylesPresentation}
+                src={img_2}
+                alt="2"
+              />
+              <img
+                style={actionStyles}
+                className={stylesPresentation}
+                src={img_3}
+                alt="3"
+              />
+              <img
+                style={actionStyles}
+                className={stylesPresentation}
+                src={img_4}
+                alt="4"
+              />
+            </div>
+            <div className={this.state.isClicked ? "text hidden" : "text"}>
+              tusk.world
+            </div>
+            <div
+              className={
+                this.state.isClicked
+                  ? "description-text"
+                  : "description-text hidden"
+              }
+            >
+              Tusk is a project I am working on that uses Node.js, Mongo,
+              Express and Socket.io. It can allow registered users to post
+              favors, chat, search, upload files and others. I am currently
+              working integrating on an admin dashboard that will monitor
+              registered users.
+            </div>
           </div>
         </div>
-        {/* 
-        <div className={styles}>
-
-          <div className="projectTitle">
-            <a href="http:/tusk.world">Tusk.world</a>
-            <div className="projectDesc">
-              Tusk is a web app I have built using Node.js, EJS and MongoDB.
-              Currently, I am rebuilding it using React instead of EJS.
-            </div>
-            <div className="projectImages">
-              <img src={img_1} alt="" />
-              <img src={img_4} alt="" />
-              <img src={img_3} alt="" />
-              <img src={img_2} alt="" />
-            </div>
-          </div>
-
-          <div className="projectTitle">
-            <a href="https://stupefied-lichterman-cd4eea.netlify.com">
-              ABC Asian
-            </a>
-            <div className="projectDesc">
-              ABC Asian is my first hands-on experience with React. It serves as
-              an e-commerce website and uses Paypal as a payment service.
-            </div>
-            <div className="projectImages">
-              <img src={img_5} alt="" />
-              <img src={img_6} alt="" />
-              <img src={img_7} alt="" />
-              <img src={img_8} alt="" />
-            </div>
-          </div> 
-        </div>
-          */}
       </div>
     );
   }
